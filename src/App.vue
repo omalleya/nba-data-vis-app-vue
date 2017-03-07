718ead1aa0e9a2b0c23dfba1c3fa9db26ddb3fa2
omalleya <omalleya@oregonstate.edu>
2017-03-06 19:14:40 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <search></search>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+import Search from './components/Search';
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Search,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <form>
+            <label for="search">Search</label>
+            <input name="search" placeholder="Lebron James" type="text" />
+            <button type="submit">Search</button>
+        </form>
+    </div>
+</template>
+
+<script type="text/javascript">
+
+    export default {
+
+    };
+
+</script>
+
+<style>
+</style>
